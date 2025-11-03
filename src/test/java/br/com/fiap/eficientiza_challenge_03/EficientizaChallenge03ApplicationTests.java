53bcf6f47261a325b2155ee89266cd8e2e599291
GuiFelSS <guilhermeflp6@gmail.com>
2025-11-03 12:41:04 -0300

    arrumando teste e variaveis de ambiente

--- src/test/java/br/com/fiap/eficientiza_challenge_03/EficientizaChallenge03ApplicationTests.java
+++ src/test/java/br/com/fiap/eficientiza_challenge_03/EficientizaChallenge03ApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.fiap.eficientiza_challenge_03;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EficientizaChallenge03ApplicationTests {
 
 	@Test
